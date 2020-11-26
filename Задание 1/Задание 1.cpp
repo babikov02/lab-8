8156e44c3d6a07a5eb4f9a3598e2108a81ac32dc
Бабиков Дмитрий <dima.babikov.02@mail.ru>
2020-11-26 21:56:28 +0300

    Добавьте файлы проекта.

---
+++ Задание 1/Задание 1.cpp
@@ -0,0 +1,35 @@
+﻿using namespace std;
+#include <iostream>
+#include<ctime>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int* m, n;
+	cout << "Введите размерность массива: " << endl;
+	cin >> n;
+	m = new int[n];
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
+	{
+		m[i] = rand() % 30;
+		cout << m[i] << " ";
+	}
+	cout << "\n";
+
+
+	int a, b;
+	cout << "Введите промежуток: ";
+	cin >> a >> b;
+	cout << "Измененный массив: ";
+	for (int i = 0; i < n; i++)
+	{
+		if (m[i] >= a && m[i] <= b)
+		{
+			m[i] = 0;
+		}
+		cout << m[i] << " ";
+	}
+	return 0;
+
+}
---
+++ Задание 2/Задание 2.cpp
@@ -0,0 +1,41 @@
+﻿using namespace std;
+#include <iostream>
+#include<ctime>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int b;
+    int c;
+
+
+    int* m, n;
+    cout << "Введите размерность массива: " << endl;
+    cin >> n;
+    m = new int[n];
+    cout << "Массив: ";
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        m[i] = rand() % 30;
+        cout << m[i] << " ";
+    }
+    cout << "\n";
+
+    cout << "Введите начало и конец интервала: ";
+    cin >> b >> c;
+
+    cout << "Элементы: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        if (m[i] < b || m[i] > c)
+        {
+            cout << m[i] << " ";
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
